b0dadd39aa3e4835dbb743ba0b0ce4ba2984578d
nyavke <nyavke@gmail.com>
2026-06-06 21:24:07 +0300

    build: PyInstaller support (detectors/native.py)

--- detectors/native.py
+++ detectors/native.py
@@ -16,7 +16,9 @@ try:
 except ImportError:
     PSUTIL_OK = False
 
-with open(Path(__file__).parent.parent / 'signatures' / 'cheats.json', encoding='utf-8') as f:
+from detectors._resources import resource_path
+
+with open(resource_path('signatures/cheats.json'), encoding='utf-8') as f:
     SIGS = json.load(f)
 
 SUSPICIOUS_DLL_KEYWORDS = [
